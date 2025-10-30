58946da98ff98c86beb11bd0dd3c166a408bad39
Francisco Marcello <fmarcello265@gmail.com>
2025-10-30 08:14:45 -0400

    Ajustando para deploy

--- src/main/kotlin/com/chico/db_inspector/web/CorsMvcConfig.kt
+++ src/main/kotlin/com/chico/db_inspector/web/CorsMvcConfig.kt
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class CorsMvcConfig : WebMvcConfigurer {
     override fun addCorsMappings(reg: CorsRegistry) {
         reg.addMapping("/api/db/**")
-            .allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200","http://localhost:8080","http://localhost:8081")
+            .allowedOrigins("http://localhost:4200", "http://127.0.0.1:4200","http://localhost:8080","http://localhost:8081","http://172.18.10.76:4080")
             .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
             .allowedHeaders("X-SQL-EXEC-URL","X-API-Token","Authorization","Content-Type","Accept","X-Requested-With")
             .exposedHeaders("Location")
